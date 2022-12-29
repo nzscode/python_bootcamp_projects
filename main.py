394dbd6d8d598d38a01765808615fbc76bebbaa0
nzscode <93976904+nzscode@users.noreply.github.com>
2022-12-29 15:48:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ shape_sides = int(input("How many sides to the shape?  \n"))
 shape_steps = int(input("How many steps per side?   \n"))
 shape_angle = 360/shape_sides
 new_angle = 0
+
 timmy = Turtle()
 timmy.shape('turtle')
 timmy.color("green")
 timmy.speed(0.51)
-timmy.pendown()
+remaining_steps = shape_steps
+dash_length = 4
+dash_segments = 25
 for i in range(shape_sides):
-    timmy.forward(shape_steps)
+    for x in range(dash_segments):
+        timmy.pendown()
+        timmy.forward(dash_length)
+        remaining_steps -= dash_length
+        timmy.penup()
+        timmy.forward(dash_length)
+        remaining_steps -= dash_length
+        dash_segments -= 2
     new_angle += shape_angle
     timmy.setheading(new_angle)
 screen = Screen()
