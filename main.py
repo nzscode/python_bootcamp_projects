e46c7acd6bc0204a96da1aecc5c3dc42b94c408a
nzscode <93976904+nzscode@users.noreply.github.com>
2022-12-29 15:16:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from turtle import Turtle, Screen
 
+shape_sides = int(input("How many sides to the shape?  \n"))
+shape_steps = int(input("How many steps per side?   \n"))
+shape_angle = 360/shape_sides
+new_angle = 0
 timmy = Turtle()
+timmy.shape('turtle')
+timmy.color("green")
+timmy.speed(0.51)
+timmy.pendown()
+for i in range(shape_sides):
+    timmy.forward(shape_steps)
+    new_angle += shape_angle
+    timmy.setheading(new_angle)
 screen = Screen()
 screen.exitonclick()
+
+
