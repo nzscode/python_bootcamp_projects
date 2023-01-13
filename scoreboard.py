586566d371f87f688e72576678ff9c31c8d391da
nzscode <93976904+nzscode@users.noreply.github.com>
2023-01-13 14:54:08 -0500

    Create scoreboard.py

---
+++ scoreboard.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+FONT = ("Courier", 50, "bold")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.color("white")
+        self.penup()
+        self.write(f"{self.score}", font=FONT)
