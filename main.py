72bed79534de510b506d21a8b5338a912a698d1a
nzscode <93976904+nzscode@users.noreply.github.com>
2023-01-16 09:10:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from turtle import Screen
-from scoreboard import Scoreboard
+from scoreboard import ScoreboardRight,ScoreboardLeft
 from midline import Midline
 from paddles import Paddle
 from ball import Ball
@@ -7,26 +7,31 @@ from ball import Ball
 
 screen = Screen()
 screen.bgcolor("black")
-screen.screensize(canvwidth=800, canvheight=400)
+screen.setup(width=800, height=600)
+screen.title("Pong!")
+screen.tracer(0)
 
-scoreboard_right = Scoreboard()
-scoreboard_right.goto(100, 0)
+scoreboard_right = ScoreboardRight()
+scoreboard_left = ScoreboardLeft()
 midline = Midline()
 midline.midline_dash()
 screen.listen()
 paddle_left = Paddle()
-paddle_left.paddle_left()
+paddle_left.paddle_start_position(-350, 0)
 paddle_right = Paddle()
-paddle_right.paddle_right()
+paddle_right.paddle_start_position(350, 0)
 ball = Ball()
 game_on = True
 
-screen.onkey(paddle_left.paddle_left_up, "w")
-screen.onkey(paddle_left.paddle_left_down, "s")
-screen.onkey(paddle_right.paddle_right_up, "Up")
-screen.onkey(paddle_right.paddle_right_down, "Down")
+screen.onkey(paddle_left.paddle_up, "w")
+screen.onkey(paddle_left.paddle_down, "s")
+screen.onkey(paddle_right.paddle_up, "Up")
+screen.onkey(paddle_right.paddle_down, "Down")
 
-# while game_on:
+while game_on:
+    screen.update()
+    screen.onkey(ball.start_angle, "c")
+
 #     ball_position = [ball.xcor(), ball.ycor()]
 #     if ball.xcor() < -300:
 #         scoreboard_left.score += 1
