ef57f361ada7cf95bc8fbaef7cb898852ffe8b5f
nzscode <93976904+nzscode@users.noreply.github.com>
2023-01-16 09:10:40 -0500

    Update paddles.py

--- paddles.py
+++ paddles.py
@@ -1,5 +1,10 @@
 from turtle import Turtle
 UP = 90
+MOVE_STEPS = 100
+STRETCH_WIDTH = 1
+STRETCH_LENGTH = 5
+MAX_Y = 240
+MIN_Y = -225
 
 
 class Paddle(Turtle):
@@ -9,23 +14,17 @@ class Paddle(Turtle):
         self.setheading(UP)
         self.color("white")
         self.shape("square")
-        self.shapesize(stretch_wid=1, stretch_len=3)
+        self.shapesize(stretch_wid=STRETCH_WIDTH, stretch_len=STRETCH_LENGTH)
         self.speed("slow")
 
-    def paddle_left(self):
-        self.goto(-300, 0)
+    def paddle_start_position(self, pos_x, pos_y):
+        self.goto(pos_x, pos_y)
 
-    def paddle_right(self):
-        self.goto(300, 0)
+    def paddle_up(self):
+        if self.ycor() < MAX_Y:
+            self.forward(MOVE_STEPS)
 
-    def paddle_left_up(self):
-        self.goto(-300, 300)
+    def paddle_down(self):
+        if self.ycor() > MIN_Y:
+            self.backward(MOVE_STEPS)
 
-    def paddle_left_down(self):
-        self.goto(-300, -300)
-
-    def paddle_right_up(self):
-        self.goto(300, 300)
-
-    def paddle_right_down(self):
-        self.goto(300, -300)
