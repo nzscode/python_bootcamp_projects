efe5c1fdebb25fb4780c07ac08394838e375a18b
nzscode <93976904+nzscode@users.noreply.github.com>
2023-01-16 22:38:13 -0500

    Update ball.py

--- ball.py
+++ ball.py
@@ -3,7 +3,7 @@ import random
 
 STRETCH_FACTOR = 1
 ANGLES = [45, 135, 225, 315]
-CORNERS = [[350, 350], [-350, 350], [-350, -350], [350, -350]]
+CORNERS = [[350, 250], [-350, 250], [-350, -350], [350, -350]]
 
 
 class Ball(Turtle):
@@ -14,6 +14,18 @@ class Ball(Turtle):
         self.shapesize(stretch_wid=STRETCH_FACTOR, stretch_len=STRETCH_FACTOR)
         self.penup()
 
-    def start_angle(self):
-        for angle in ANGLES:
+
+    def move_ball(self):
+        # new_angle = random.choice(ANGLES)
+        # if new_angle in ANGLES:
+        #     angle_index = ANGLES.index(new_angle)
+        #     new_corner = CORNERS[angle_index]
+        #     print(new_corner)
+        #     self.setheading(new_angle)
+        #     self.goto(new_corner[0], new_corner[1])
+        #     self.speed(0.1)
+        self.speed("slowest")
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+        self.goto(new_x, new_y)
             
