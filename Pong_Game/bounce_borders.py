f4d4b5d1a8aab22f684d76590ddb5d8e50d75a51
nzscode <93976904+nzscode@users.noreply.github.com>
2023-01-20 12:27:23 -0500

    Create bounce_borders.py

---
+++ Pong_Game/bounce_borders.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+
+
+class Border(Turtle):
+    def __init__(self, start_x, start_y, end_x, end_y):
+        super().__init__()
+        self.hideturtle()
+        self.color("white")
+        self.shape("square")
+        self.penup()
+        self.goto(start_x, start_y)
+        self.pendown()
+        self.goto(end_x, end_y)
+
+
