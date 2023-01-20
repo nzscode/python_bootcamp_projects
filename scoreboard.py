71df01521387fbb31cd251f46d9efcab0751db98
nzscode <93976904+nzscode@users.noreply.github.com>
2023-01-19 22:51:12 -0500

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 FONT = ("Courier", 50, "bold")
 
 
-class ScoreboardRight(Turtle):
+class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
@@ -12,13 +12,3 @@ class ScoreboardRight(Turtle):
         self.goto(30, 200)
         self.write(f"{self.score}", font=FONT)
 
-
-class ScoreboardLeft(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.score = 0
-        self.hideturtle()
-        self.color("white")
-        self.penup()
-        self.goto(-75, 200)
-        self.write(f"{self.score}", font=FONT)
