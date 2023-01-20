d7a1194594ac3477b5c57c7745c1334d930bc9d2
nzscode <93976904+nzscode@users.noreply.github.com>
2023-01-19 22:51:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import time
 from turtle import Screen
-from scoreboard import ScoreboardRight, ScoreboardLeft
+from scoreboard import Scoreboard
 from midline import Midline
 from paddles import Paddle
 from ball import Ball
-from bounce_borders import Border
+import random
+
+ANGLES = [45, 135, 225, 315]
+new_angle = random.choice(ANGLES)
 
 screen = Screen()
 screen.bgcolor("black")
@@ -12,17 +15,14 @@ screen.setup(width=800, height=600)
 screen.title("Pong!")
 screen.tracer(0)
 
-scoreboard_right = ScoreboardRight()
-scoreboard_left = ScoreboardLeft()
+scoreboard_right = Scoreboard()
 midline = Midline()
 midline.midline_dash()
-border_top = Border(-395, 298, 395, 298)
-border_bottom = Border(-395, -290, 395, -290)
 screen.listen()
 paddle_left = Paddle()
-paddle_left.paddle_start_position(-350, 0)
+paddle_left.paddle_start_position(-380, 0)
 paddle_right = Paddle()
-paddle_right.paddle_start_position(350, 0)
+paddle_right.paddle_start_position(380, 0)
 ball = Ball()
 game_on = True
 
@@ -31,17 +31,16 @@ screen.onkey(paddle_left.paddle_down, "s")
 screen.onkey(paddle_right.paddle_up, "Up")
 screen.onkey(paddle_right.paddle_down, "Down")
 
+
 while game_on:
     time.sleep(0.1)
     screen.update()
     ball.move_ball()
-    if paddle_left.distance(ball) > 5 and ball.xcor() > 300 and ball.ycor() < 250 or ball.ycor() > -250:
-        scoreboard_left.score += 1
-    elif paddle_right.distance(ball) > 5 and ball.xcor() > -300 and ball.ycor() < 250 or ball.ycor() > -250:
-        scoreboard_right.score += 1
 
-    if ball.ycor() > 298 and ball.xcor() > 0:
-        ball.setheading(275)
-        ball.move_ball()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(paddle_right) < 50 and ball.xcor() > 340 or ball.distance(paddle_left) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
 
 screen.exitonclick()
