0b39d729ca7f2853e2655dace55f3c6eea310d20
nzscode <93976904+nzscode@users.noreply.github.com>
2023-01-20 20:37:38 -0500

    Update player.py

--- player.py
+++ player.py
@@ -1,19 +1,7 @@
-from turtle import Turtle
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
 
 
-class Player(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.shape("turtle")
-        self.shapesize(stretch_wid=2, stretch_len=2)
-        self.penup()
-        self.hideturtle()
-        self.setheading(90)
-        self.setposition(0, -250)
-        self.showturtle()
-
-    def player_move(self):
-        self.forward(20)
-
-    def player_reset(self):
-        self.goto(0, -250)
+class Player:
+    pass
