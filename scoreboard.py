92cf42673c5fd1365a3b2384dc4a9df7575e26e5
nzscode <93976904+nzscode@users.noreply.github.com>
2023-01-20 20:37:41 -0500

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,17 +1,5 @@
-from turtle import Turtle
+FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.hideturtle()
-        self.penup()
-        self.setposition(0, 250)
-        self.score = 0
-
-    def increase_score(self):
-        self.score += 1
-        self.clear()
-
-    def update_scoreboard(self):
-        self.write(f"{self.score}")
+class Scoreboard:
+    pass
