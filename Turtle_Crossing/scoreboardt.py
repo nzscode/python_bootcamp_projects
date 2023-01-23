e31cbb93493758283dd5d09a0cd2779fad395758
nzscode <93976904+nzscode@users.noreply.github.com>
2023-01-23 17:25:07 -0500

    Create scoreboardt.py

---
+++ Turtle_Crossing/scoreboardt.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.level = 1
+        self.goto(-200, 250)
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def update_level(self):
+        self.level += 1
+        self.clear()
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align="center", font=FONT)
+
+
