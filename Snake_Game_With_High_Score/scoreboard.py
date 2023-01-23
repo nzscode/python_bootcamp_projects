39ccd18714aad958bae7a98c1b118c07e815ba0b
nzscode <93976904+nzscode@users.noreply.github.com>
2023-01-23 18:34:23 -0500

    Update scoreboard.py

--- Snake_Game_With_High_Score/scoreboard.py
+++ Snake_Game_With_High_Score/scoreboard.py
@@ -5,20 +5,29 @@ FONT = ("Courier", 20, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
         self.penup()
         self.hideturtle()
+        self.score = 0
+        with open("snake_high_score.txt") as data:
+            self.highscore = int(data.read())
+        print(self.highscore)
         self.goto(0, 250)
         self.color("white")
 
-    def increase_score(self):
+    def increase_score(self): 
         self.score += 1
         self.clear()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"Game Over!", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("snake_high_score.txt", mode="w") as new_high_score:
+                new_high_score.write(str(self.highscore))
+        self.score = 0
+        self.update_scoreboard()
+
