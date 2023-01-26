bc01e96e783f30dc5d882542ddcdb1bae98ba00f
nzscode <93976904+nzscode@users.noreply.github.com>
2023-01-25 22:00:46 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+import pandas as pd
+
+
