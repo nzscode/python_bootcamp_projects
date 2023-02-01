3b4c00dd8f08380a0a9edbc60aed039424a17c9d
nzscode <93976904+nzscode@users.noreply.github.com>
2023-02-01 09:49:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-# with open("weather_data.csv") as data_file:
-#     data = data_file.readlines()
-import pandas
-# import csv
-#
-# with open("weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != "temp":
-#             temperatures.append(int(row[1]))
-#     print(temperatures)
+
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-# print(data["temp"])
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
-data_dict = data.to_dict()
-print(data_dict)
-data_list = data["temp"].to_list()
-max = data["temp"].max()
-print(max)
+fur_colors = data["Primary Fur Color"].value_counts()
+print(fur_colors)
+
