4b090c26c5fb786a11945710ebb7985a322646cd
nzscode <93976904+nzscode@users.noreply.github.com>
2023-02-02 10:37:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
-
-
+import turtle
+from turtle import Turtle, Screen
+from scoreboard import Scoreboard
 import pandas
 
-data = pandas.read_csv("Squirrel_Count/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+screen = Screen()
+screen.title("U.S. States Naming Game")
 
-grey_squirrels = data[data["Primary Fur Color"] == "Gray"]
-grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
-cinnamon_squirrels = data[data["Primary Fur Color"] == "Cinnamon"]
-cinnamon_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
-black_squirrels = data[data["Primary Fur Color"] == "Black"]
-black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
-print(f"Gray squirrels: {grey_squirrels_count}\nRed squirrels: {cinnamon_squirrels_count}\n"
-      f"Black squirrels: {black_squirrels_count}")
 
-data_dict = {
-      "Fur Color": ["Gray", "Red", "Black"],
-      "Quantity": [grey_squirrels_count, cinnamon_squirrels_count, black_squirrels_count]
-}
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+scoreboard = Scoreboard()
+data = pandas.read_csv("50_states.csv")
+state_name_list = data["state"].to_list()
+input_list = []
+new_turtle = Turtle()
+new_turtle.hideturtle()
+new_turtle.penup()
 
-df = pandas.DataFrame(data_dict)
-df.to_csv("Squirrel_Color_Counts.csv")
 
-# fur_colors = data["Primary Fur Color"].value_counts()
-# print(fur_colors)
+game_over = False
+
+
+def game():
+    answer_state = screen.textinput("State Input: ", "Name a State: ").lower()
+    proper_case_state = answer_state.title()
+    state = data[data.state == proper_case_state]
+    new_x = state.x
+    new_y = state.y
+
+    if proper_case_state in state_name_list:
+        if proper_case_state not in input_list:
+            new_turtle.goto(float(new_x), float(new_y))
+            new_turtle.write(proper_case_state, font=("Courier", 8, "bold"))
+            scoreboard.score += 1
+            scoreboard.update_scoreboard()
+            input_list.append(proper_case_state)
+        elif proper_case_state in input_list:
+            pass
+    else:
+        scoreboard.wrong_answers += 1
+        scoreboard.update_scoreboard()
+    game()
+
+
+while not game_over:
+    game()
+    if scoreboard.score == 50 | scoreboard.wrong_answers == 10:
+        game_over = True
+
+
+screen.exitonclick()
