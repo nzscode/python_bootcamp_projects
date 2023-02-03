39f449ca725c54186d95980dfb7cfd6b28723fd5
nzscode <93976904+nzscode@users.noreply.github.com>
2023-02-02 20:52:35 -0500

    Update main.py

--- US_States_Naming_Game/main.py
+++ US_States_Naming_Game/main.py
@@ -6,61 +6,59 @@ screen = Screen()
 screen.title("U.S. States Naming Game")
 
 
-image = "blank_states_img.gif"
+image = "US_States_Naming_Game/blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 data = pandas.read_csv("US_States_Naming_Game/50_states.csv")
 state_name_list = data["state"].to_list()
 input_list = []
-new_turtle = Turtle()
-new_turtle.hideturtle()
-new_turtle.penup()
 scoreboard = Turtle()
 scoreboard.penup()
 scoreboard.hideturtle()
 score = 0
-wrong_answers = 0
 scoreboard.goto(0, 350)
-scoreboard.write(f"Score: {score}/50 Wrong Answers: {wrong_answers}/10", align="center", font=("Courier", 30, "bold"))
-
+scoreboard.write(f"Score: {score}/50", align="center", font=("Courier", 30, "bold"))
+new_state_dict = {
+    "state name": []
+}
 game_over = False
 
 
 while not game_over:
-    answer_state = screen.textinput("State Input: ", "Name a State: ").lower()
-    trimmed_state = answer_state.strip()
-    proper_case_state = trimmed_state.title()
-    state = data[data.state == proper_case_state]
+    answer_state = screen.textinput("State Input: ", "Name a State: ").title()
+    state = data[data.state == answer_state]
     new_x = state.x
     new_y = state.y
 
-    if proper_case_state in state_name_list:
-        if proper_case_state not in input_list:
+    if answer_state in state_name_list:
+        if answer_state not in input_list:
+            new_turtle = Turtle()
+            new_turtle.hideturtle()
+            new_turtle.penup()
             new_turtle.goto(float(new_x), float(new_y))
-            new_turtle.write(proper_case_state, font=("Courier", 8, "bold"))
+            new_turtle.write(answer_state, font=("Courier", 8, "bold"))
             score += 1
-            input_list.append(proper_case_state)
+            input_list.append(answer_state)
             scoreboard.clear()
-            scoreboard.write(f"Score: {score}/50 Wrong Answers: {wrong_answers}/10",
+            scoreboard.write(f"Score: {score}/50",
                              align="center", font=("Courier", 30, "bold"))
-        elif proper_case_state in input_list:
+        elif answer_state in input_list:
             pass
-    else:
-        wrong_answers += 1
-        scoreboard.clear()
-        scoreboard.write(f"Score: {score}/50 Wrong Answers: {wrong_answers}/10",
-                         align="center", font=("Courier", 30, "bold"))
 
-    if wrong_answers > 10:
+    if answer_state == "Exit":
         for state in data.state:
             if state not in input_list:
                 final_turtle = Turtle()
-                final_turtle.penup()
                 final_turtle.hideturtle()
+                final_turtle.penup()
+                final_turtle.color("red")
                 missing_state = data[data.state == state]
                 final_turtle.goto(float(missing_state.x), float(missing_state.y))
                 final_turtle.write(state, font=("Courier", 8, "bold"))
+                new_state_dict["state name"].append(state)
                 game_over = True
 
+df = pandas.DataFrame(new_state_dict)
+df.to_csv("missed_states.csv")
 
 screen.exitonclick()
