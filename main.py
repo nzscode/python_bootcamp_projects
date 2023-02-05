a9932160418665f2cd07af80e11d41f699e7beaf
nzscode <93976904+nzscode@users.noreply.github.com>
2023-02-05 12:43:10 -0500

    Update main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import random
-import pygame
-
-names = ["Tom", "Ron", "Bob", "Jane"]
-student_scores = {student: random.randint(1, 100) for student in names}
-
-for key in student_scores:
-    print(student_scores.fromkeys(key))
