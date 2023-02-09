92d40e3396d1d0a83fe0b467d2c80f318615b900
nzscode <93976904+nzscode@users.noreply.github.com>
2023-02-09 10:27:19 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter
+window = tkinter.Tk()
+
+window.title("Test 1")
+window.minsize(300, 100)
+
+## Label
+my_label = tkinter.Label(text="I am a label.", font=("Courier", 20, "bold"))
+my_label.pack()
+my_label
+
+
+
+
+
+window.mainloop()
