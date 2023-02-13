11d7ed9f5d5cba3bee41f23c3473fe21d18a4eb3
nzscode <93976904+nzscode@users.noreply.github.com>
2023-02-13 10:35:38 -0500

    Update playground.py

--- playground.py
+++ playground.py
@@ -5,4 +5,34 @@ def add(*args):
     return sum
 
 
-print(add(1, 2, 3))
+# print(add(1, 2, 3))
+
+
+def calculate(n, **kwargs):
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.windows = kw.get("windows")
+        self.wheels = kw.get("wheels")
+        self.car_type()
+        self.wheel_multiply()
+
+    def wheel_multiply(self):
+        if self.wheels:
+            print(self.wheels * 10)
+
+    def car_type(self):
+        if self.make == "Toyota":
+            print("Toyota: Camri")
+
+
+car = Car(make="Toyota", wheels=3)
+print(f"Windows = {car.windows}")
