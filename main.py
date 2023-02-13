ec0d1e4bc5096c243d1fadc70c0bd0d5702fd713
nzscode <93976904+nzscode@users.noreply.github.com>
2023-02-13 17:53:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,45 +5,46 @@ window.title("Test 1")
 window.minsize(300, 100)
 
 ##Label
-my_label = Label(text="I am a label.", font=("Courier", 20, "bold"))
+my_label = Label(text="Result will go here.", font=("Courier", 18, "bold"))
 my_label.pack()
 
-my_label["text"] = "Hello, I am Labello."
-my_label.config(text="Hello, I'm Larabel.")
-
-##Button
-
-
-def button_clicked():
-    my_label.config(text="Button got clicked.")
-
-
-button = Button(text="Click Me", command=button_clicked)
-button.pack()
-
 ## Entry
-input = Entry()
-input.pack()
+us_label1 = Label(text="Please enter the 1st number below:")
+us_label1.pack()
+us_input1 = Entry()
+us_input1.pack()
 
-
-def Print_Response():
-    user_input = input.get()
-    my_label.config(text=user_input)
-
-
-print_response_button = Button(text="Print", command=Print_Response)
-print_response_button.pack()
+us_label2 = Label(text="Please enter the 2nd number below:")
+us_label2.pack()
+us_input2 = Entry()
 
 def calculate():
-    user_input = input.get()
-    result = int(user_input) + 5
-    my_label.config(text=result)
+    user_input1 = us_input1.get()
+    user_input2 = us_input2.get()
+    print(radio_state)
+    if radio_state.get() == 1:
+        result = int(user_input1) + int(user_input2)
+        my_label.config(text=result)
+    elif radio_state.get() == 2:
+        result = int(user_input1) - int(user_input2)
+        my_label.config(text=result)
+    elif radio_state.get() == 3:
+        result = int(user_input1) * int(user_input2)
+        my_label.config(text=result)
+    else:
+        result = int(user_input1) / int(user_input2)
+        my_label.config(text=result)
 
 
-calculate_button = Button(text="Calculate", command=calculate)
-calculate_button.pack()
-
-
+radio_state = IntVar()
+radiobutton_add = Radiobutton(text="+ or add", value=1, variable=radio_state, command=calculate)
+radiobutton_add.pack()
+radiobutton_minus = Radiobutton(text="- or subtract", value=2, variable=radio_state, command=calculate)
+radiobutton_minus.pack()
+radiobutton_multiply = Radiobutton(text="* or multiply", value=3, variable=radio_state, command=calculate)
+radiobutton_multiply.pack()
+radiobutton_divide = Radiobutton(text="/ or divide", value=4, variable=radio_state, command=calculate)
+radiobutton_divide.pack()
 
 
 
