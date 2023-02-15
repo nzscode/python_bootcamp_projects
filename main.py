0dd8b73b742186b50d1afd964d2893a9e06993cf
nzscode <93976904+nzscode@users.noreply.github.com>
2023-02-15 15:29:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import *
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
+YELLOW = "#FCFFE7"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -18,8 +18,8 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, bg="#FCFFE7")
-canvas = Canvas(width=200, height=230, bg="#FCFFE7", highlightthickness=0)
+window.config(padx=100, pady=50, bg=YELLOW)
+canvas = Canvas(width=200, height=230, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 115, image=tomato_img)
 canvas.create_text(100, 135, text="00:00", font=(FONT_NAME, 30, "bold"), fill="white")
