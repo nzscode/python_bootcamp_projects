5a9b2da5b351e59dcb7f0c04a6002f9933a9c81e
nzscode <93976904+nzscode@users.noreply.github.com>
2023-02-27 10:19:53 -0500

    Update Testing_Ground.py

--- Testing_Ground.py
+++ Testing_Ground.py
@@ -1,5 +1,35 @@
-def color_invert(r,g,b):
-	print(b,g,r)
+def parity_analysis(num):
+    new_num = num
+    num = str(num)
+    total = 0
+    for n in num:
+        total += int(n)
+        print(total)
 
+    total_is = ""
+    num_is = ""
+    if total % 2 == 0:
+        total_is = "even"
+    else:
+        total_is = "odd"
 
-color_invert(165, 172, 119)
+    if new_num % 2 == 0:
+        num_is = "even"
+    else:
+        num_is = "odd"
+
+    if total_is == num_is:
+        print("True")
+    else:
+        print("False")
+
+    # if new_num % 2 == 0 & total % 2 == 0:
+    #     print("Parity")
+    # elif new_num % 2 != 0 & total % 2 != 0:
+    #     print("Parity")
+    # elif new_num % 2 == 0 & total % 2 != 0:
+    #     print("Nope")
+    # elif new_num % 2 != 0 & total % 2 == 0:
+    #     print("Nope")
+
+parity_analysis(243)
