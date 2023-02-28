f671213b9b9e04e09d837d131e283b2aa514e14e
nzscode <93976904+nzscode@users.noreply.github.com>
2023-02-28 09:44:36 -0500

    Update Testing_Ground.py

--- Testing_Ground.py
+++ Testing_Ground.py
@@ -1,35 +1,49 @@
-def parity_analysis(num):
-    new_num = num
-    num = str(num)
-    total = 0
-    for n in num:
-        total += int(n)
-        print(total)
+# import pandas
+#
+# nato_data_frame = pandas.read_csv("NATO_Alphabet/nato_phonetic_alphabet.csv")
+# continue_translating = True
+# translation = []
+#
+#
+# def translate():
+#     user_input = input("What is your word? \n").upper().strip()
+#     for char in user_input:
+#         for (index, row) in nato_data_frame.iterrows():
+#             if row.letter == char:
+#                 # print(row.code)
+#                 translation.append(row.code)
+#     print(translation)
+#
+#
+# def translate_again():
+#     global continue_translating
+#     user_response = input("Would you like to translate another word? Y or N\n").upper().strip()
+#     if user_response == "N":
+#         continue_translating = False
+#     elif user_response != "Y":
+#         print("Sorry, please choose a valid response. Y or N.")
+#         translate_again()
+#
+#
+# while continue_translating:
+#     translate()
+#     translate_again()
 
-    total_is = ""
-    num_is = ""
-    if total % 2 == 0:
-        total_is = "even"
-    else:
-        total_is = "odd"
 
-    if new_num % 2 == 0:
-        num_is = "even"
-    else:
-        num_is = "odd"
+# Use a nested list comprehension to find all of the numbers from 1–1000
+# that are divisible by any single digit besides 1 (2–9)
+dividers = [2, 3, 4, 5, 6, 7, 8, 9]
+nums = [num for num in range(1, 21) for div in dividers if num % div == 0]
+print(nums)
 
-    if total_is == num_is:
-        print("True")
-    else:
-        print("False")
+new_i_list = []
+for i in range(20, 31):
+    string_i = str(i)
+    str_i_sum = int(string_i[0]) + int(string_i[1])
+    if str_i_sum % 2 == 0:
+        new_i_list.append(i)
+print(new_i_list)
 
-    # if new_num % 2 == 0 & total % 2 == 0:
-    #     print("Parity")
-    # elif new_num % 2 != 0 & total % 2 != 0:
-    #     print("Parity")
-    # elif new_num % 2 == 0 & total % 2 != 0:
-    #     print("Nope")
-    # elif new_num % 2 != 0 & total % 2 == 0:
-    #     print("Nope")
+new_j_list = [j for j in range(20, 31) if (int(str(j)[0]) + int(str(j)[1])) % 2 != 0]
+print(new_j_list)
 
-parity_analysis(243)
