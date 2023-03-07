47ce9e9342f7aaac2d7cd7af1ade43b5a26addc0
nzscode <93976904+nzscode@users.noreply.github.com>
2023-03-07 10:47:54 -0500

    Update Testing_Ground.py

--- Testing_Ground.py
+++ Testing_Ground.py
@@ -1,19 +1,39 @@
-Recipes = {}
-recipe_name = input("what is the name of your recipe?\n").title()
+from tkinter import *
 
-add_ingredient = True
+window = Tk()
+
+window.title("Recipe Maker.")
+window.minsize(width=200, height=200)
+window.config(padx=50, pady=50)
+
+recipe_name_label = Label(text="Recipe Name: ")
+recipe_name_label.grid(column=0, row=0, pady=10)
+
+recipe_name_entry = Entry()
+# recipe_name_entry.insert(END, "Enter recipe name here.")
+recipe_name_entry.grid(column=1, row=0, columnspan=3)
+
+ingredient_label = Label(text="Ingredient")
+ingredient_label.grid(column=0, row=1, pady=10)
+
+measure_label = Label(text="measure")
+measure_label.grid(column=1, row=1)
+
+quantity_label = Label(text="quantity")
+quantity_label.grid(column=2, row=1)
+
+add_ingredient_button = Button(text= "+")
+add_ingredient_button.grid(column=3, row=1)
+
+remove_ingredient_button = Button(text="-")
+remove_ingredient_button.grid(column=4, row=1)
 
 
-def enter_ingredients():
-    ingredient = input("What is your ingredient?\n").lower()
-    q_name = input("grams or cups\n").lower()
-    quantity = input(f"How many {q_name} of {ingredient}?\n").lower()
 
-while add_ingredient:
-    a_ingredient = input("Add ingredient? Y or N\n").lower()
-    if add_ingredient == "y":
-        enter_ingredients()
-    else:
-        add_ingredient = False
 
-enter_ingredients()
+
+
+
+
+
+window.mainloop()
