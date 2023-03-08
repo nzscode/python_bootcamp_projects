e8eae814094c991caee14d48ef1c5f6d38733a09
nzscode <93976904+nzscode@users.noreply.github.com>
2023-03-08 09:02:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from tkinter import *
 import pandas
 import random
 data = pandas.read_csv("french_words.csv")
+fr_eng_dict = data.to_dict(orient="records")
+print(fr_eng_dict)
 BACKGROUND_COLOR = "#B1DDC6"
 LANGUAGE = "French"
 LANG_FONT = ("Ariel", 40, "italic")
@@ -14,19 +16,23 @@ window.title("Flash Cards")
 window.minsize(width=1000, height=800)
 window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
 
+
 def new_card():
-    french_word = random.choice(data.French)
-    # english_word = data[data.French == french_word]
-    # anglais = english_word.English
-    # english_word = data[data.French == french_word]
+    rand_dict = random.choice(fr_eng_dict)
+    rand_dict_fr_word = rand_dict["French"]
+    rand_dict_en_word = rand_dict["English"]
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=rand_dict_fr_word)
 
 
-canvas1 = Canvas(width=900, height=600, bg=BACKGROUND_COLOR, highlightthickness=0)
+
+
+canvas = Canvas(width=900, height=600, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_card_img = PhotoImage(file="card_front.png")
-canvas1.create_image(450, 300, image=front_card_img)
-canvas1.grid(column=0, row=0, columnspan=2)
-canvas1.create_text(450, 200, text=LANGUAGE, font=LANG_FONT)
-canvas1.create_text(450, 300, font=WORD_FONT)
+canvas.create_image(450, 300, image=front_card_img)
+canvas.grid(column=0, row=0, columnspan=2)
+card_title = canvas.create_text(450, 200, text="", font=LANG_FONT)
+card_word = canvas.create_text(450, 300, text="", font=WORD_FONT)
 
 
 wrong_button_image = PhotoImage(file="wrong.png")
@@ -37,4 +43,5 @@ right_button_image = PhotoImage(file="right.png")
 right_button = Button(image=right_button_image, highlightthickness=0, bd=0, command=new_card)
 right_button.grid(column=1, row=2)
 
+new_card()
 window.mainloop()
