41ebda77e8786a5175de07efecd067618d5bdf68
nzscode <93976904+nzscode@users.noreply.github.com>
2023-03-08 18:50:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,69 @@
 from tkinter import *
 import pandas
 import random
-data = pandas.read_csv("french_words.csv")
-fr_eng_dict = data.to_dict(orient="records")
-print(fr_eng_dict)
+
 BACKGROUND_COLOR = "#B1DDC6"
 LANGUAGE = "French"
 LANG_FONT = ("Ariel", 40, "italic")
-
 WORD_FONT = ("Ariel", 60, "bold")
 
+# data = pandas.read_csv("french_words.csv")
+data = pandas.read_csv("tests.csv")
+fr_eng_dict = data.to_dict(orient="records")
 
 window = Tk()
 window.title("Flash Cards")
 window.minsize(width=1000, height=800)
 window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
 
-chosen_word = ""
+rand_dict = {}
 def new_card():
+    global rand_dict, flip_timer
+    window.after_cancel(flip_timer)
     rand_dict = random.choice(fr_eng_dict)
     rand_dict_fr_word = rand_dict["French"]
-    rand_dict_en_word = rand_dict["English"]
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=rand_dict_fr_word)
+    canvas.itemconfig(card_image, image=front_card_img)
+    canvas.itemconfig(card_title, text="French", fill="Black")
+    canvas.itemconfig(card_word, text=rand_dict_fr_word, fill="Black")
+    flip_timer = window.after(3000, card_flip)
+
+
 
 def card_flip():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    rand_dict_en_word = rand_dict["English"]
+    canvas.itemconfig(card_word, text=rand_dict_en_word)
+    canvas.itemconfig(card_image, image=back_card_img)
+    canvas.itemconfig(card_word, fill="white")
 
+def remove_from_dict():
+    global fr_eng_dict
+    new_dict = fr_eng_dict.remove(rand_dict)
+    to_learn = pandas.DataFrame(new_dict)
+    to_learn.to_csv("words_To_Learn.csv")
+    print(fr_eng_dict)
 
-
-
-
-window.after(3000, card_flip)
+flip_timer = window.after(3000, card_flip)
 canvas = Canvas(width=900, height=600, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_card_img = PhotoImage(file="card_front.png")
-canvas.create_image(450, 300, image=front_card_img)
+back_card_img = PhotoImage(file="card_back.png")
+card_image = canvas.create_image(450, 300)
 canvas.grid(column=0, row=0, columnspan=2)
 card_title = canvas.create_text(450, 200, text="", font=LANG_FONT)
 card_word = canvas.create_text(450, 300, text="", font=WORD_FONT)
-
 
 wrong_button_image = PhotoImage(file="wrong.png")
 wrong_button = Button(image=wrong_button_image, highlightthickness=0, bd=0, command=new_card)
 wrong_button.grid(column=0, row=2)
 
 right_button_image = PhotoImage(file="right.png")
-right_button = Button(image=right_button_image, highlightthickness=0, bd=0, command=new_card)
+right_button = Button(image=right_button_image, highlightthickness=0, bd=0,
+                      command=lambda: [new_card(), remove_from_dict()])
 right_button.grid(column=1, row=2)
+
 
 new_card()
 window.mainloop()
+
+
+
