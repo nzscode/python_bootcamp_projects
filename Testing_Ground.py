09c15db57ecfc96144946c0d28d44d16508afed6
nzscode <93976904+nzscode@users.noreply.github.com>
2023-04-24 21:24:58 -0400

    Update Testing_Ground.py

--- Testing_Ground.py
+++ Testing_Ground.py
@@ -1,4 +1,22 @@
-num = 2.37194165512
-print(f'{num:.2f}')
-print(round(num, 3))
+import random
+word_list = ["apple", "banana", "cantaloupe", "durian", "elderberry", "figs"]
+
+comp_word = random.choice(word_list)
+comp_word_blanks = []
+print(comp_word)
+
+for a in comp_word:
+    comp_word_blanks.append("_")
+
+print(comp_word_blanks)
+user_choice = input("Choice?\n")
+if user_choice in comp_word:
+    print(f"Good guess. There is a '{user_choice}' in the word.")
+    for user_choice in comp_word:
+        alpha_index = comp_word.index(user_choice)
+        print(alpha_index)
+
+print(comp_word_blanks)
+
+
 
