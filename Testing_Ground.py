18802e361fc0f6525c258b9e63f336672cce991e
nzscode <93976904+nzscode@users.noreply.github.com>
2023-04-29 10:24:09 -0400

    Update Testing_Ground.py

---
+++ Testing_Ground.py
@@ -0,0 +1,37 @@
+# person = {"name": "Sara", "age": 29}
+# print(person)
+# print(person["name"])
+# print(person.values())
+# person["name"] = "Yolanda"
+# print(person)
+# person["pets"] = ["Kitty", "Doggo"]
+# print(person)
+# person.pop("pets")
+# print(person)
+#
+# for key in person:
+#     print(key)
+#     print(person[key])
+#
+# synonyms = {
+#     "mountain": "peak",
+#     "forest": "jungle"
+# }
+# print("1", )
+
+#looping through a dictionary
+dict = {
+    "a": "apple",
+    "b": "banana",
+    "c": "cherry",
+    "d": "date",
+    "e": "lalala"
+}
+
+for key in dict:
+    print(key)
+
+for key in dict:
+    print(f"{dict[key]}")
+
+
