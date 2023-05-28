0d7ce9212da3247255d83081624710feb5de9382
nzscode <93976904+nzscode@users.noreply.github.com>
2023-05-28 00:30:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-class Apple:
-    def __init__(self, color, flavor):
-        self.color = color
-        self.flavor = flavor
-
-    def __str__(self):
-        return "This apple is {} and its flavor is {}". format(self.color, self.flavor)
-
-jonagold = Apple("red", "sweet")
-print(jonagold)
+names = ["Kelly", "Amy", "Joanne", "Tracy", "Lois"]
+print(sorted(names, key=len))
