b58c2e3789e0f26a601bffc5d52a34a78c40cdcd
nzscode <93976904+nzscode@users.noreply.github.com>
2023-05-27 23:11:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-def counter(start, stop):
-    if start > stop:
-        return_string = "Counting down: "
-        while start != stop: # Complete the while loop
-            return_string += str(start) # Add the numbers to the "return_string"
-            if start > stop:
-                return_string += ","
-            start- 1 # Increment the appropriate variable
-    else:
-        return_string = "Counting up: "
-        while start != stop: # Complete the while loop
-            return_string += str(start) # Add the numbers to the "return_string"
-            if start < stop:
-                return_string += ","
-            start += 1 # Increment the appropriate variable
-    return return_string
+class Apple:
+    def __init__(self, color, flavor):
+        self.color = color
+        self.flavor = flavor
 
+    def __str__(self):
+        return "This apple is {} and its flavor is {}". format(self.color, self.flavor)
 
-print(counter(1, 10)) # Should be "Counting up: 1,2,3,4,5,6,7,8,9,10"
-print(counter(2, 1)) # Should be "Counting down: 2,1"
-print(counter(5, 5)) # Should be "Counting up: 5"
+jonagold = Apple("red", "sweet")
+print(jonagold)
