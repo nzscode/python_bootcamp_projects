f9d03b10c39dd7f48d776502a742ed3b06b48299
nzscode <93976904+nzscode@users.noreply.github.com>
2023-06-08 17:06:38 -0400

    Create calc.py

---
+++ Testing/calc.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    if type(a) != int or type(b) != int:
+        return "Not valid numbers"
+    return a+b
+
+def multiply(a, b):
+    if type(a) != int or type(b) != int:
+        return "Not valid numbers"
+    return a*b
+
+def divide(a, b):
+    if type(a) != int or type(b) != int:
+        return "Not valid numbers"
+    return a/b
+
+def subtract(a, b):
+    if type(a) != int or type(b) != int:
+        return "Not valid numbers"
+    return a-b
