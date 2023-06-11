6c33eb0fc6cf0589fa676b6007478a8076592616
nzscode <93976904+nzscode@users.noreply.github.com>
2023-06-11 15:46:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+import re
+
+my_txt = "An investment in knowledge pays the best interest."
+
+
+def LetterCompiler(txt):
+    result = re.findall(r'([a-c]).', txt)
+    return result
+
+
+print(LetterCompiler(my_txt))("hello world")
