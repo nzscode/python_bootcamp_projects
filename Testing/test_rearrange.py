f28dc4866ddb2962cb9d7c62c6d74e47ebd0ee40
nzscode <93976904+nzscode@users.noreply.github.com>
2023-06-11 15:47:17 -0400

    Create test_rearrange.py

---
+++ Testing/test_rearrange.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from rearrange import rearrange_name
+
+class Test(TestCase):
+    def test_rearrange_name(self):
+        testcase = "Lovelace, Ada"
+        expected = "Ada Lovelace"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_one_name(self):
+        testcase = "Laila"
+        expected = "Laila"
+        self.assertEqual(rearrange_name(testcase), expected)
