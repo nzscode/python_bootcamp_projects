ddad8a8c653be5c70e8ea94146609a95e267a525
nzscode <93976904+nzscode@users.noreply.github.com>
2023-06-14 09:24:17 -0400

    Create test_emails.py

---
+++ ImplementingUnitTesting_Google_Qwiklabs/test_emails.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from ImplementingUnitTesting_Google_Qwiklabs.emails import find_email
+
+
+class Test(TestCase):
+    def test_find_email(self):
+        testcase = [None, "Bree", "Campbell"]
+        expected = "breee@abc.edu"
+        self.assertEqual(find_email(testcase), expected)
+
+    def test_one_name(self):
+        testcase = [None, "John"]
+        expected = "Missing parameters"
+        self.assertEqual(find_email(testcase), expected)
+
+    def test_two_name(self):
+        testcase = [None, "Roy", "Cooper"]
+        expected = "No email address found"
+        self.assertEqual(find_email(testcase), expected)
