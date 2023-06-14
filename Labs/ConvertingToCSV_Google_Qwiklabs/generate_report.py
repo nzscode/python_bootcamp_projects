76ca3f3073e9f74a254ca1a28be4fbfc85236ba1
nzscode <93976904+nzscode@users.noreply.github.com>
2023-06-14 14:35:19 -0400

    Create generate_report.py

---
+++ Labs/ConvertingToCSV_Google_Qwiklabs/generate_report.py
@@ -0,0 +1,66 @@
+import csv
+
+##PART 1:
+# def read_employees(csv_file_location):
+#     csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
+#     employee_file = csv.DictReader(open(csv_file_location), dialect='empDialect')
+#     employee_list = []
+#     for data in employee_file:
+#         employee_list.append(data)
+#     return employee_list
+#
+# employee_list = read_employees('../employees.csv')
+# print(employee_list)
+
+# #PART 2:
+# def read_employees(csv_file_location):
+#     csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
+#     employee_file = csv.DictReader(open(csv_file_location), dialect='empDialect')
+#     employee_list = []
+#     for data in employee_file:
+#         employee_list.append(data)
+#     return employee_list
+#
+# employee_list = read_employees('../employees.csv')
+#
+# def process_data(employee_list):
+#     department_list = []
+#     for employee_data in employee_list:
+#         department_list.append(employee_data['Department'])
+#     department_data = {}
+#     for department_name in set(department_list):
+#         department_data[department_name] = department_list.count(department_name)
+#     return department_data
+#
+# dictionary = process_data(employee_list)
+# print(dictionary)
+
+#PART 3:
+def read_employees(csv_file_location):
+    csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
+    employee_file = csv.DictReader(open(csv_file_location), dialect='empDialect')
+    employee_list = []
+    for data in employee_file:
+        employee_list.append(data)
+    return employee_list
+
+employee_list = read_employees('../data/employees.csv')
+
+def process_data(employee_list):
+    department_list = []
+    for employee_data in employee_list:
+        department_list.append(employee_data['Department'])
+    department_data = {}
+    for department_name in set(department_list):
+        department_data[department_name] = department_list.count(department_name)
+    return department_data
+
+dictionary = process_data(employee_list)
+
+def write_report(dictionary, report_file):
+    with open(report_file, "w+") as f:
+        for k in sorted(dictionary):
+            f.write(str(k) + ':' + str(dictionary[k]) + '\n')
+        f.close()
+
+write_report(dictionary, '../data/report.txt')
