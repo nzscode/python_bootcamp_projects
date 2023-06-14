82a63f69d1af04f894dece491b0fb991ad0d5cca
nzscode <93976904+nzscode@users.noreply.github.com>
2023-06-14 09:24:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import re
+def validate_user(username, minlen):
+    assert type(username) == str, "username must be a string"
+    assert minlen > 0, "minlen must be at least 1"
+    if minlen < 1:
+        raise ValueError("minlen must be at least 1")
+    if len(username) < minlen:
+        return False
+    if not username.isalnum():
+        return False
+    return True
 
-my_txt = "An investment in knowledge pays the best interest."
-
-
-def LetterCompiler(txt):
-    result = re.findall(r'([a-c]).', txt)
-    return result
-
-
-print(LetterCompiler(my_txt))("hello world")
+print(validate_user("rurjhgiofjo", 5))
